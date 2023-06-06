0e12bf3a478ba1fbe7bfc546c85fcae92c01ff8f
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-06-06 13:51:58 +0530

    Debouncing in ReactJS

--- src/components/Home/Home1.jsx
+++ src/components/Home/Home1.jsx
@@ -5,6 +5,10 @@ const Home1 = () => {
 
   const [nameVal, setNameVal] = useState("");
 
+
+  /* The useEffect is getting called when the value of state variable nameVal get changes. This state variable is assignmed to value in input
+     if we type a in input then the useEffect gets called with the setTimeout of 1sec of fetching the email from API for nameVal= a then if we again append b to a i.e ab then the useEffect() will return the callback function which clears the timeout and then again renders the useEffect()  after 1sec shows the results for the input ab
+  */
   useEffect(() => {
 
      const timeOutId =  setTimeout(async () => {
